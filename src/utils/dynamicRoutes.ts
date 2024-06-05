c6d3ac59569c6af1aafdd74f7fb5e3e94cd9d044
zdenham <zdenham1@gmail.com>
2024-06-05 12:45:45 -0700

    fix handful of type errors

---
+++ src/utils/dynamicRoutes.ts
@@ -0,0 +1,1 @@
+export const dynamicRoutes: string[] | null = null;
--- src/utils/useDynamicParams.ts
+++ src/utils/useDynamicParams.ts
@@ -6,6 +6,11 @@ import { extractParamsFromPath } from './extractParamsFromPath';
 export const useDynamicParams = (): Record<string, string> => {
   const pathName = usePathname();
 
+  if (!dynamicRoutes)
+    throw new Error(
+      'No dynamic routes found, you likely need to run next-static-utils generate'
+    );
+
   const params = extractParamsFromPath(pathName, dynamicRoutes) || {};
 
   return params;
